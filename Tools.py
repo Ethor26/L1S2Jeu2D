c3b50f0ed0e39fe2f72c4649fd64dc5760cdfccc
Ethan SUISSA <ethan.suissa@efrei.net>
2021-02-21 21:43:58 +0100

    Ajout fichier scores Maj Commentaires

--- Tools.py
+++ Tools.py
@@ -1,22 +1,8 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("hello")
-# Importation de pyplot avec matplotlib
-from matplotlib import pyplot
-
-print(pyplot.plot([1, 2, 3, 6], [1, 4, 9, 36]))
-
+## ======================================================
+# PROJET TRANSVERSE 1 L1 : Jeu 2D Python
+# Auteurs : Equipe ShirosakiBest = Ethan SUISSA, Lilandra ALBERT-LAVAULT, Pierre REY, Jean-Alexis TADDEI, Ludwig
+# NEUBERTH- EFREI -L1 BN
+# Date : 4 mai 2021 (?)
+# Fichier outils
+# ======================================================
+print("Welcome to the game")
