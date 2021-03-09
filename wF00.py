629f4481fe088fb7f685555347782c5a693f8e6c
Lilandraa <lilandralavault@gmail.com>
2021-03-09 19:28:46 +0100

    resolution alignement gitub

--- wF00.py
+++ wF00.py
@@ -32,22 +32,26 @@ class F00(Tk):
         # ...........< L A B E L S > .........................
         # ELEMENT GRAPHIQUE : <Label> = [Libellé T01] : ...??
         # ??? A FAIRE
-        lbl=Label(self,text="Il y a bien longtemps, une galaxie lointaine était dirigée par un Conseil\n"
-            "de sages qui s’efforçaient à ce que tous le monde vive une vie paisible.\n"
-            "Malheureusement, ils virent bientôt apparaître une armée menée par un\n"
-            "empereur infecté par un virus nommé Galacticov. Ce virus avait paralysé\n"
-            "toutes les zones du cerveau de l’empereur qui lui permettait d’éprouver\n"
-            "des émotions négatives et stimulé les connexions dans celles créant des\n"
-            "émotions négatives. Un empereur juste et intègre devint alors cruel et\n "
-            "implacable.Ce virus étant contrôlé à distance par un conseiller avide\n "
-            "de pouvoir dans une centrale, le Conseil vous recruta avec d’autres héros\n"
-            "pour atteindre et détruire cette centrale, en évitant les tirs des vaisseaux\n"
-            "délégué par l’empereur. Vous, <Nom à définir>, devrez donc passer à travers \n" 
-            "cette armée, mais avec l’interdiction du Conseil de détruire les autres vaisseaux.\n" 
-            "En effet, ils sont contrôlés par des pilotes innocents mais manipulés. Ils ignorent\n" 
-            "vos objectifs et s’imaginent que vous venez détruire leur empire. Une quête ardue \n" 
-            "commence pour vous…\n" ,font='Arial 30 bold',bg='black',fg='white' )
+
+        # Affichagage du sénario
+
+        lbl = Label(self, text="Il y a bien longtemps, une galaxie lointaine était dirigée par un Conseil\n"
+                               "de sages qui s’efforçaient à ce que tous le monde vive une vie paisible.\n"
+                               "Malheureusement, ils virent bientôt apparaître une armée menée par un\n"
+                               "empereur infecté par un virus nommé Galacticov. Ce virus avait paralysé\n"
+                               "toutes les zones du cerveau de l’empereur qui lui permettait d’éprouver\n"
+                               "des émotions négatives et stimulé les connexions dans celles créant des\n"
+                               "émotions négatives. Un empereur juste et intègre devint alors cruel et\n "
+                               "implacable.Ce virus étant contrôlé à distance par un conseiller avide\n "
+                               "de pouvoir dans une centrale, le Conseil vous recruta avec d’autres héros\n"
+                               "pour atteindre et détruire cette centrale, en évitant les tirs des vaisseaux\n"
+                               "délégué par l’empereur. Vous, <Nom à définir>, devrez donc passer à travers \n"
+                               "cette armée, mais avec l’interdiction du Conseil de détruire les autres vaisseaux.\n"
+                               "En effet, ils sont contrôlés par des pilotes innocents mais manipulés. Ils ignorent\n"
+                               "vos objectifs et s’imaginent que vous venez détruire leur empire. Une quête ardue \n"
+                               "commence pour vous…\n", font='Arial 30 bold', bg='black', fg='white')
         lbl.pack(padx=5, pady=5)
+        # .pack sert à placer le texte
         # ELEMENT GRAPHIQUE : <Label> = [Libellé T02] : ...??
         # ??? A FAIRE
 
