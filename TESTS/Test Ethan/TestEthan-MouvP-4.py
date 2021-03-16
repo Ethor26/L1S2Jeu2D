daabfa8de8646fd3e8d5633a0b08751600fcb8cd
Ethan SUISSA <ethan.suissa@efrei.net>
2021-03-16 23:17:51 +0100

    Continuation Commande Programmable, Fin d'ajustement de l'angle et création de la trajectoire courbe et progressive. Réglage Vitesse en cours.

--- TESTS/Test Ethan/TestEthan-MouvP-4.py
+++ TESTS/Test Ethan/TestEthan-MouvP-4.py
@@ -1,14 +1,17 @@
 import math
+# import time
 
+
+print("Welcome to TestEthan-MouvP-4")
 from tkinter import *
 from math import *
 
-Largeur = 480  # Largeur Ecran Jeu
-Hauteur = 320  # Hauteur Ecran Jeu
+Largeur = 1000  # Largeur Ecran Jeu
+Hauteur = 500 # Hauteur Ecran Jeu
 Rayon = 10  # rayon de l'objet
-ValeurPas = 4  # Nombre de pas de déplacement
 
-AngleEnDegree = 350
+t = 0
+AngleEnDegree = 0
 
 # position initiale du pion
 PosX = 230
@@ -33,20 +36,34 @@ def CommandeClavier(event):
     # A CODER
 
     if touche == 'p':
-        Mafenetre.after(100, deplacement_P)   # Lance la fonction toute les 100 ms
+        deplacement_P()
+
+
 
 # =============================================================================
 # Fonction de déplacement de la touche P
 def deplacement_P():
-    global PosX, PosY
-    for i in range(3):
-        PosX, PosY = ValeurPosXY(PosX, PosY, ValeurPas + i)
+    global PosX, PosY,t
+    nbRebond = 0
+    t = t + 0.0001
+
+    PosX, PosY, siRebond, t = ValeurPosXY(PosX, PosY, t)
 
     print("posY = ", PosY)  # pour controle
     print("posX = ", PosX)  # pour controle
     print("Pion = ", Pion)  # pour controle
+    print("t = ", t)
+
     Canevas.coords(Pion, PosX - 10, PosY - 10, PosX + 10, PosY + 10)
 
+    if siRebond:
+        nbRebond = nbRebond + 1
+        print("Nombre de rebond = ", nbRebond)
+
+    idAfter=Mafenetre.after(3,deplacement_P)
+
+    if nbRebond > 0 or t > 0.05:
+        Mafenetre.after_cancel(idAfter)
 
 # FONCTION OUTIL: Calcul de la commande programmable. Auteur : Ethan SUISSA - En cours
 def ValeurAngleParametreEnRadian():
@@ -57,46 +74,54 @@ def ValeurAngleParametreEnRadian():
 
 
 # Utilise l'équation de mouvement pour calculer la postion finale en fonction de l'angle
-def CalcProg(AngleDeduitDegree, posXFinal):
-    v0 = 1  # Choix de vitesse à 5
+def CalcProg(AngleDeduitDegree, t):
+    v0 = 0.9  # Choix de vitesse à 5
     g = 9.81  # Constante de gravitation
     Angle_Deduit_Radian = math.radians(AngleDeduitDegree)  # convertion en Radian de l'angle deduit
-    Eqmouv = (-1 / 2) * ((g * posXFinal ** 2) / (v0 * cos(Angle_Deduit_Radian)) ** 2) + tan(
-        Angle_Deduit_Radian) * posXFinal
-    AdaptEqMouv = int(Eqmouv)
-    return AdaptEqMouv
+    dx = v0 * cos(Angle_Deduit_Radian)
+    if Angle_Deduit_Radian == 0.0:
+        g = 0
+    dy = -g * t + v0 * sin(Angle_Deduit_Radian)
+    print("Angle radian:", Angle_Deduit_Radian) # Test
+    return dx, dy
 
-def ValeurPosXY(valX_Initial, valY_Initial, NbPas):
-    global valY_Final, valX_Final
+
+def ValeurPosXY(valX_Initial, valY_Initial, t):
+    global valY_Final, valX_Final, rebond
     print("ValPosXY :  valXInitial =", valX_Initial)
     print("ValPosXY :  valYInitial =", valY_Initial)
+    rebond = False
 
     # Bloc 1
-    if 0 < AngleEnDegree < 89:
+    if 0 <= AngleEnDegree <= 90:
         print("ValPosXY :  Bloc 1 ")  # pour controle
-        valX_Final = valX_Initial + NbPas
-        valY_Final = CalcProg(AngleEnDegree, NbPas)
+        Dx, Dy = CalcProg(AngleEnDegree, t)
+        valX_Final = valX_Initial + Dx
+        valY_Final = valY_Initial - Dy
 
     # Bloc 2
-    if 90 < AngleEnDegree < 180:
+    if 90 < AngleEnDegree <= 180:
         print("ValPosX :  Bloc 2 ")  # pour controle
         Angle_Deduit_Degree = AngleEnDegree - 90
-        valY_Final = valX_Initial + NbPas
-        valX_Final = -CalcProg(Angle_Deduit_Degree, NbPas)
+        Dx, Dy = CalcProg(Angle_Deduit_Degree, t)
+        valX_Final = valX_Initial - Dy
+        valY_Final = valY_Initial - Dx
 
     # Bloc 3
-    if 180 < AngleEnDegree < 270:
+    if 180 < AngleEnDegree <= 270:
         print("ValPosX :  Bloc 3 ")  # pour controle
         Angle_Deduit_Degree = AngleEnDegree - 180
-        valX_Final = valX_Initial - NbPas
-        valY_Final = -CalcProg(Angle_Deduit_Degree, NbPas)
+        Dx, Dy = CalcProg(Angle_Deduit_Degree, t)
+        valX_Final = valX_Initial - Dx
+        valY_Final = valY_Initial + Dy
 
     # Bloc 4
-    if 270 < AngleEnDegree < 360:
+    if 270 < AngleEnDegree <= 360:
         print("ValPosX :  Bloc 4  ")  # pour controle
         Angle_Deduit_Degree = AngleEnDegree - 270
-        valY_Final = valY_Initial - NbPas
-        valX_Final = CalcProg(Angle_Deduit_Degree, NbPas)
+        Dx, Dy = CalcProg(Angle_Deduit_Degree, t)
+        valX_Final = valX_Initial + Dy
+        valY_Final = valY_Initial + Dx
 
     print("ValPosXY : ValXFinal avant correction = ", valX_Final)
     print("ValPosXY : ValYFinal avant correction = ", valY_Final)
@@ -106,21 +131,27 @@ def ValeurPosXY(valX_Initial, valY_Initial, NbPas):
     if valX_Final > Largeur - Rayon:
         print("ValPosXY (posX):  Sortie du cadre à droite => Rebond ")
         valX_Final = valX_Initial - Rayon
+        rebond = True
+
     # Rebond à gauche
     if valX_Final < Rayon:
         print("ValPosXY (posX):  Sortie du cadre à gauche => Rebond ")
         valX_Final = valX_Initial + Rayon
+        rebond = True
 
     # Rebond en bas
     if valY_Final < Rayon:
         print("ValPosXY (posY):  Sortie en bas ==> Rebond")
         valY_Final = valY_Initial + Rayon
+        rebond = True
+
     # Rebond en haut
     if valY_Final > Hauteur - Rayon:
         print("ValPosXY (posY):  Sortie en haut ==> Rebond")
         valY_Final = valY_Initial - Rayon
+        rebond = True
 
-    return valX_Final, valY_Final
+    return valX_Final, valY_Final, rebond, t
 
 
 # Création de la fenêtre principale
@@ -139,3 +170,37 @@ Canevas.pack(padx=5, pady=5)
 Button(Mafenetre, text='Quitter', command=Mafenetre.destroy).pack(side=LEFT, padx=5, pady=5)
 
 Mafenetre.mainloop()
+
+# GRAVITE = 9.8
+# ANGLE = PI / 4
+# V0 = 0.8
+
+# def setup():
+#     global largeur
+#     global hauteur
+#     global x
+#     global y
+#     global dx
+#     global dy
+#     global t
+#     largeur = 1000
+#     hauteur = 500
+#     size(1000, 500)
+#     x = 0
+#     y = hauteur / 2
+#     dy = 0
+#     t = 0
+#     frameRate(100)
+#
+#
+# def draw():
+#     global x, y, t
+#     background(200)
+#     noStroke()
+#     fill(255, 0, 0)
+#     ellipse(x, y, 10, 10)
+#     dx = - V0 * cos(PI - ANGLE)
+#     dy = GRAVITE * t - V0 * sin(PI - ANGLE)
+#     x = x + dx
+#     y = y + dy
+#     t = t + 0.0001
