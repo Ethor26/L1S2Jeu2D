e943d78d9789b905b837425dd356c1b018306a1e
Pierre <pierre.rey@efrei.net>
2021-03-23 20:42:20 +0100

    image5

--- TESTS/TestPierre2.py
+++ TESTS/TestPierre2.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from wF00 import *
+from wF01 import *
+from wF02 import *
 #Fonction qui s'affiche au lancement du main
 def F00():
-    self= Tk()
+    self = Tk()
     self.title("F00")
     photo = PhotoImage(file="../IMAGES/Image F00/image5.png")
     canvas = Canvas(self, width=1200, height=700)
@@ -23,7 +26,7 @@ def F00():
                        "vos objectifs et s’imaginent que vous venez détruire leur empire. Une quête ardue \n"
                        "commence pour vous…\n", font='Gabriola 17 italic', fill='white')
     b1 = Button(self, text="Close", command=self.destroy).pack(side=RIGHT)
-    b2 = Button(self, text="Play", command=self).pack(side=LEFT)
+    b2 = Button(self, text="Play", command=self.commandeOuvreF01).pack(side=LEFT)
     label = Label(self, text="Saisir votre pseudo", bg="white").pack(side=BOTTOM)
     value = StringVar()
     value.set("texte par défaut")
