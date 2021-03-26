9d2e06e29f3c058cd0b4f1daebe0c8619224efca
jatad <jean-alexis.taddei@efrei.net>
2021-03-26 13:40:04 +0100

    Maj open_score_file FONCTIONNELLE + best score comparaison ect

--- score_tab.py
+++ score_tab.py
@@ -1,7 +1,7 @@
 #f = nom du fichier
-from wF00 import *
 
 def open_score_file():
+
     with open("scores.txt", 'r') as filin:
         score = filin.readlines()
         nb_line = len(score)
@@ -16,7 +16,6 @@ def open_score_file():
                 tab[j]= spel
     return tab
 
-
 def ajout_score(f, id, user, angle, score):
     with open(f, 'a') as file:
         file.write('\n')
@@ -27,16 +26,6 @@ def ajout_score(f, id, user, angle, score):
         file.write(angle)
         file.write(';')
         file.write(score)
-
-def score_comparaison(score_player, best_score):
-    if score_player < best_score:
-        #nouveau label"vous ferez mieux la prochaine fois", ("Vous etes à" 'best_score-scoreplayer' du meilleur score)
-    if score_player > best_score:
-        #nouveau label "Bravo vous etes le nouveau meilleur gardien de la galaxie"
-
-        # Il faut convertir les int en str avant d'écrire dans le fichier
-        # f= nom du fichier
-
 
 def ajout_nom_F000(name):
     with open("scores.txt", 'r') as file:
@@ -51,16 +40,44 @@ def ajout_nom_F000(name):
         f.write(name)
         f.write(";")
 
-
 def ajout_angle_F02(angle):
     with open("scores.txt", "a") as file:
         ang = str(angle)
         file.write(ang)
         file.write(";")
 
-
 def ajout_score_F0(score):          #fenetre de jeu
     with open("scores.txt", "a") as f:
         sco = str(score)
         f.write(sco)
 
+
+def score_comparaison1():
+    #on regarde dans le txt quel est le meilleur score on doit utiliser la fonction open_score_file afin d'utiliser le tableau avec le score
+    best_score = 0
+    with open("scores.txt", 'r') as filin:
+            score = filin.readlines()
+            nb_line = len(score)
+            tab = nb_line * [0]
+            for i in range(len(tab)):
+                tab[i] = nb_line * [0]
+            for i in range(nb_line):
+                score[i] = score[i].rstrip('\n')
+                for j in range(nb_line):
+                    spell = score[j]
+                    spel = spell.split(";")
+                    tab[j] = spel
+
+    print(tab)
+    for i in range(1, len(tab)-1):
+        temp_score = tab[i][3]
+        temp_score2 = int(temp_score)
+        if best_score < temp_score2:
+            best_score = temp_score2
+    temp_perso_score = tab[nb_line-1][3]
+    temp_pers = int(temp_perso_score)
+    if temp_pers > best_score:
+        best_score = temp_pers
+    return best_score
+
+print(score_comparaison1())
