4208619beb00473ed6bb583a0fb7e9947dc08e63
jatad <jean-alexis.taddei@efrei.net>
2021-03-26 09:36:55 +0100

    Fonction score_tab

--- score_tab.py
+++ score_tab.py
@@ -1,19 +1,12 @@
 #f = nom du fichier
 def open_score_file(f):
-    score_tab = []
     with open(f, 'r') as filin:
         score = filin.readlines()
-        while score != ' ':
-            print(score)
-        for i in score:
-            i.split()
-    for ligne in f:
-        score_tab[i]
-        temp_tab =  ligne.split()
-        for i in score_tab:
-            score_tab[i] == temp_tab
+        nb_line = len(score)
+        for i in range(nb_line):
+            score[i] = score[i].rstrip('\n')
+    return score
 
-    return score_tab
 
 def ajout_score(f, id, user, angle, score):
     with open(f, 'a') as file:
@@ -27,10 +20,10 @@ def ajout_score(f, id, user, angle, score):
         file.write(score)
 
 def score_comparaison(score_tab, best_score):
-    if score_tab[4] < best_score:
-        #fonction ouverture fenetre "vous ferez mieux la prochaine fois"
-    if score_tab[4] > best_score:
-        #fonction ouverture fenetre "Bravo vous etes le nouveau meilleur gardien de la galaxie"
+    if score_player < best_score:
+        #nouveau label"vous ferez mieux la prochaine fois", ("Vous etes à" 'best_score-scoreplayer' du meilleur score)
+    if score_player > best_score:
+        #nouveau label "Bravo vous etes le nouveau meilleur gardien de la galaxie"
 
         # Il faut convertir les int en str avant d'écrire dans le fichier
         # f= nom du fichier
